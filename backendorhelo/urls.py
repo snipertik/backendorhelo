b615dc236e3a9d21f5e3405e9b6392e906c2509e
snipertik <sniperinformatik@gmail.com>
2025-07-23 07:56:37 +0000

    Ajout route de test /ping/

--- backendorhelo/urls.py
+++ backendorhelo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('transfert.urls')),  # <<== Lien vers nos APIs
+    path('ping/', lambda request: JsonResponse({'message': 'pong !'})),
 ]
